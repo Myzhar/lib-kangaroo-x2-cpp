8deb699211fec850db428b3249e2ddd878244ea4
Walter Lucetti <walter.lucetti@stereolabs.com>
2024-03-10 12:16:23 +0100

    fix `close` visibility

--- include/Stream.hpp
+++ include/Stream.hpp
@@ -22,12 +22,12 @@ public:
   bool openSerialPort(
     std::string & port_name,
     const LibSerial::BaudRate & baudrate = LibSerial::BaudRate::BAUD_1152000);
+  void close();
 
   ssize_t writeBuffer(const uint8_t * buffer, size_t lengthOfBuffer);
   bool readByte(uint8_t & data);
 
 protected:
-  void close();
 
 private:
   std::shared_ptr<LibSerial::SerialPort> _serial;
